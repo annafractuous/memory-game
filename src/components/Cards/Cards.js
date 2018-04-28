1e07b0fd164a5d21830e1563747763429fdd3a96
annafractuous <anna.rasshivkina@flatironschool.com>
2018-04-28 00:45:44 -0400

    move Card component into Cards file

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import Card from '../Card/Card'
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props)
+		this.state = {
+			flipped: false
+		}
+
+		this.handleClick = this.handleClick.bind(this)
+    }
+
+	handleClick(e) {
+		this.props.handleClick(e)
+	}
+
+	render() {
+		let cardClass = ''
+		cardClass += 'card-container'
+		cardClass += this.state.flipped ? ' flipped' : ''
+		cardClass += this.props.active ? ' active' : ''
+		
+		return (
+			<div className={cardClass} data-value={this.props.value} onClick={this.handleClick}>
+				<div className='card'>
+					{this.props.value}
+				</div>
+			</div>
+		)
+	}
+}
+Card.propTypes = {
+    value: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
+    handleClick: PropTypes.func.isRequired
+}
 
 class Cards extends React.Component {
     constructor(props) {
