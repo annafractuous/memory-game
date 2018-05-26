d88c2ac182ea22843b3d34b2d2aa5a396951f776
annafractuous <anna.rasshivkina@flatironschool.com>
2018-05-26 16:07:47 -0400

    clean up App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Message from '../Message/Message'
 import Game from '../Game/Game'
+
 import { connect } from 'react-redux'
 import { toggleShowGame } from '../../redux/actions/game-state'
 import store from '../../redux/store/index'
@@ -26,37 +27,13 @@ const mapDispatchToProps = dispatch => {
     }
 }
 class ConnectedApp extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			gamePlay: false,
-			gameState: 'start',
-            totalTime: '0:00',
-            totalMoves: 0
-		}
-
-        this.setMoves = this.setMoves.bind(this)
-        this.setTime = this.setTime.bind(this)
-        this.handleUserSelection = this.handleUserSelection.bind(this)
+	constructor(props) {
+		super(props)
 	}
 
     componentWillReceiveProps(nextProps) {
         this.showGameCheck(nextProps)
     }
-
-	handleUserSelection(selection) {
-        switch (selection.type) {
-            case 'difficulty':
-			    this.selectDifficulty(selection.value)
-                break
-            case 'background':
-                this.selectBackground(selection.value)
-                break
-            case  'replay':
-			    this.restartGame()
-                break
-        }
-	}
 
     showGameCheck(nextProps) {
         if (!nextProps.gameOver && nextProps.background !== '' && nextProps.difficulty !== '') {
@@ -67,55 +44,13 @@ class ConnectedApp extends React.Component {
         }
     }
 
-	restartGame() {
-		this.setState({
-			gameState: 'start',
-            totalTime: '0:00',
-            totalMoves: 0
-		})
-	}
-
-    setMoves(moves) {
-        this.setState({
-            totalMoves: moves
-        }, this.completeGame)
-    }
-
-    setTime(time) {
-        this.setState({
-            totalTime: time
-        }, this.completeGame)
-    }
-
-	completeGame() {
-        if (this.state.totalMoves !== 0 && this.state.totalTime !== '0:00') {
-            // this.setState({
-            //     gamePlay: false,
-            //     gameState: 'complete',
-            //     difficulty: '',
-            //     background: ''
-            // })
-            this.props.toggleShowGame(false)
-        }
-	}
-
 	render() {
         // window.store = store;
         const bgColor = this.props.showGame ? gameStyles[this.props.background].pageColor : 'white'
         const style = {
             backgroundColor: bgColor
         }
-        console.log(this.props)
-        const component = this.props.showGame ? 
-            <Game 
-                setMoves={this.setMoves} 
-                setTime={this.setTime} 
-            /> :
-            <Message 
-                gameState={this.state.gameState} 
-                moves={this.state.totalMoves} 
-                time={this.state.totalTime} 
-            />
+        const component = this.props.showGame ? <Game /> : <Message />
 		
         return (
             <div className={styles.memoryGame} style={style}>
@@ -126,8 +61,12 @@ class ConnectedApp extends React.Component {
 }
 ConnectedApp.propTypes = {
     showGame: PropTypes.bool.isRequired,
-    // difficulty: PropTypes.string.isRequired,
-    background: PropTypes.string.isRequired
+    gameOver: PropTypes.bool.isRequired,
+    difficulty: PropTypes.string.isRequired,
+    background: PropTypes.string.isRequired,
+    totalTime: PropTypes.string.isRequired,
+    totalMoves: PropTypes.number.isRequired,
+    toggleShowGame: PropTypes.func.isRequired
 }
 const App = connect(mapStateToProps, mapDispatchToProps)(ConnectedApp)
 
